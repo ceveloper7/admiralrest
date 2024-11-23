48718b7a9444e936f2e53964156e5070f14828d2
ceveloper7 <cevabarcvilla@gmail.com>
2024-11-23 01:19:32 -0300

    REST version for ERP ADmiral - DataSource Hibernate

--- src/main/java/com/admiral/rest/AdmiralRestApplication.java
+++ src/main/java/com/admiral/rest/AdmiralRestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdmiralrestApplication {
+public class AdmiralRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdmiralrestApplication.class, args);
+		SpringApplication.run(AdmiralRestApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/admiral/rest/config/BasicDataSourceConfig.java
@@ -0,0 +1,4 @@
+package com.admiral.rest.config;
+
+public class BasicDataSourceConfig {
+}
